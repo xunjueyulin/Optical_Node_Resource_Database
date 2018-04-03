26916dfe15d02eb565831052d3d18b28fe4fdf5f
xunjueyulin <xunjueyulin@hotmail.com>
2018-04-03 09:26:20 +0800

    add design plan&database script
    数据库设计想法和数据库脚本

---
+++ nodedata.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+import sqlite3
+connection = sqlite3.connect('optical_node_data.sqlite')
+cursor = connection.cursor()
+cursor.execute("""CREATE TABLE optical_nodes(
+				  id INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE NOT NULL,
+				  device_type TEXT NOT NULL,
+				  headend TEXT NOT NULL,
+				  device_address TEXT NOT NULL,
+				  name TEXT NOT NULL,
+				  signalling_time DATE NOT NULL,
+				  project_number TEXT NOT NULL,
+				  device_state TEXT NOT NULL,
+				  manager TEXT NOT NULL )""")
+				  
+connection.commit()
+connection.close()
